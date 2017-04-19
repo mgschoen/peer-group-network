327f3a290c83eec2b3a2afb89c81f015e271d374
Martin Schoen <schoenm@cip.ifi.lmu.de>
2017-04-19 23:13:01 +0200

    [cms] implemented basic angular routing

---
+++ client/cms/cms.config.js
@@ -0,0 +1,45 @@
+angular.module('baemselcampCms')
+  .config(['$locationProvider', '$routeProvider',
+    function config ($locationProvider, $routeProvider) {
+
+      $locationProvider.hashPrefix('');
+
+      $routeProvider
+        .when('/login', {
+          templateUrl: './login/login.template.html'
+        })
+        .when('/editor', {
+          templateUrl: './editor-matrix/editor-matrix.template.html'
+        })
+        .when('/editor/matrix', {
+          templateUrl: './editor-matrix/editor-matrix.template.html'
+        })
+        .when('/editor/persons', {
+          templateUrl: './editor-persons-list/editor-persons-list.template.html'
+        })
+        .when('/editor/persons/list', {
+          templateUrl: './editor-persons-list/editor-persons-list.template.html'
+        })
+        .when('/editor/persons/new', {
+          templateUrl: './editor-persons-new/editor-persons-new.template.html'
+        })
+        .when('/editor/persons/edit', {
+          templateUrl: './editor-persons-edit/editor-persons-edit.template.html'
+        })
+        .when('/editor/relations', {
+          templateUrl: './editor-relations-list/editor-relations-list.template.html'
+        })
+        .when('/editor/relations/list', {
+          templateUrl: './editor-relations-list/editor-relations-list.template.html'
+        })
+
+        .when('/editor/relations/new', {
+          templateUrl: './editor-relations-new/editor-relations-new.template.html'
+        })
+        .when('/editor/relations/edit', {
+          templateUrl: './editor-relations-edit/editor-relations-edit.template.html'
+        })
+        .otherwise('/editor');
+
+    }]
+  );
---
+++ client/cms/cms.module.js
@@ -0,0 +1,1 @@
+angular.module('baemselcampCms', ['ngRoute']);
