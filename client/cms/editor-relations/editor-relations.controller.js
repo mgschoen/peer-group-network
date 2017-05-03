a113f922fada6c6a776c757509342cbd1947f016
Martin Schoen <schoenm@cip.ifi.lmu.de>
2017-05-04 00:15:13 +0200

    [cms] retrieve relationTypes list from api

--- client/cms/editor-relations/editor-relations.controller.js
+++ client/cms/editor-relations/editor-relations.controller.js
@@ -1,3 +1,17 @@
-angular.module('baemselcampCms').controller('RelationsController', function($scope){
+angular.module('baemselcampCms')
+  .controller('RelationsController', function($scope, $http){
 
-});
+    var init = function () {
+      $http.get('/api/relationtypes/').then(
+        function (response) {
+          $scope.relationTypes = response.data;
+        },
+        function (error) {
+          console.log(error);
+        }
+      );
+    };
+
+    init();
+
+  });
